d8c803b23b4d9ef2e31a62ed4039c00c53bee0be
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-09-30 23:59:27 +0900

    window and change color with if-else

--- restart/nApp.js
+++ restart/nApp.js
@@ -1,17 +1,35 @@
 const title = document.querySelector(".hello h1");
 
 function titleClicked() {
-    title.style.color = "Blue";
+    const currentColor = title.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "red";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor
 }
 
-function mouseEnter() {
-    title.innerText = "Mouse is here"
-}
+// function mouseEnter() {
+//     title.innerText = "Mouse is here"
+// }
 
-function mouseLeave() {
-    title.innerText = "Mouse is gone"
-}
+// function mouseLeave() {
+//     title.innerText = "Mouse is gone"
+// }
+
+// function resized() {
+//     document.body.style.backgroundColor = "tomato";
+// }
+
+// function copied() {
+//     alert("Do Not Copy!");
+// }
 
 title.addEventListener("click", titleClicked);
-title.addEventListener("mouseenter", mouseEnter);
-title.addEventListener("mouseleave", mouseLeave);
+// title.addEventListener("mouseenter", mouseEnter);
+// title.addEventListener("mouseleave", mouseLeave);
+
+// window.addEventListener("resize", resized);
+// window.addEventListener("copy", copied);
